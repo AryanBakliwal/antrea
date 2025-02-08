9be71f84498ded59869d273ef2f4aec9f1a8c6f7
Aryan Bakliwal <aryanbakliwal12345@gmail.com>
2025-02-08 20:34:54 +0000

    Complete LFX Mentorship 2025_01 test task
    Signed-off-by: Aryan Bakliwal <aryanbakliwal12345@gmail.com>

--- cmd/antrea-controller/controller.go
+++ cmd/antrea-controller/controller.go
@@ -124,6 +124,10 @@ var allowedPaths = []string{
 // run starts Antrea Controller with the given options and waits for termination signal.
 func run(o *Options) error {
 	klog.InfoS("Starting Antrea Controller", "version", version.GetFullVersion())
+
+	// Test log
+	klog.InfoS("LFX Mentorship 2025_01 test task log", "GitHub username", "AryanBakliwal", "Pod", env.GetPodName())
+
 	// Create K8s Clientset, Aggregator Clientset, CRD Clientset and SharedInformerFactory for the given config.
 	// Aggregator Clientset is used to update the CABundle of the APIServices backed by antrea-controller so that
 	// the aggregator can verify its serving certificate.
